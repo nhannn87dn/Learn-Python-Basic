0c41e81e3d09906ef855d7cf22b7e7d8704779d8
Nhan <nhannn87dn@gmail.com>
2023-07-18 11:38:16 +0700

    add ss6

--- Session-01/hello.py
+++ Session-01/hello.py
@@ -1,14 +1,2 @@
-class Vehicle:
-    def __init__(self, brand, color):
-            self.brand = brand
-            self.color = color
-    def drive(self):
-           print(f"The {self.color} {self.brand} is driving.") 
-
-class Car(Vehicle):
-      def honk(self): 
-            print("Beep beep!") 
-
-class Bike(Vehicle):
-         def ring_bell(self):
-               print("Ring ring!")
+if 'apple' in mylist:
+print(mylist[4:])
