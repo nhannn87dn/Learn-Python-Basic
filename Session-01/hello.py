b1ab6fddc257b092650c56348322b1be5aa4fc2b
Nhan <nhannn87dn@gmail.com>
2023-07-31 17:14:42 +0700

    up

--- Session-01/hello.py
+++ Session-01/hello.py
@@ -1,6 +1,9 @@
-import math
-x = math.sqrt(64)
-x = math.ceil(1.4)
-y = math.floor(1.4)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.items()
+
 print(x)
-print(x) # returns 2print(y) # returns 1
---
+++ Session-11/README.md
@@ -0,0 +1,1 @@
+# Ôn tập và làm bài tập
