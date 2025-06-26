7f06a4f9dfae194b41c54aaba033689a2ff6582c
nhannn87dn <nhannn87dn@gmail.com>
2025-06-26 08:13:24 +0700

    ss6

---
+++ Python-25/Session-06/bien.py
@@ -0,0 +1,16 @@
+# Câu 1: Đặt 2 biến tương ứng với tên trường, tên lớp. 
+# Sau đó in ra.
+
+# Câu 2: Đặt 2 biến x và y với giá trị bất kỳ bạn muốn.
+# Sau đó in ra tổng của x và y.
+
+# Câu 3: Cho n bằng một giá trị số nhập từ người dùng.
+# Kiểm tra n:
+# - Nếu n = 0 thì in ra n là số 0,
+# - Nếu n > 0 thì in ra n là số dương,
+# - Nếu n < 0 thì in ra n là số âm,
+
+# Câu 4: Cho m bằng một giá trị số nhập từ người dùng.
+# Kiểm tra m:
+# - Nếu m là số chẵn thì in ra m là số chẵn
+# - CÒn ngược lại in ra m là số lẻ.
